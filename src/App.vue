256cfb9224a9e0869da771d55ab15b99c4d87818
MiMi <janicehsu890228@gmail.com>
2023-11-27 19:17:28 +0800

    Liai Group Home Page  Hero Section Finished

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import Tanuki from './views/Tanuki.vue'
 </script>
 
 <template>
   <div>
-    <Tanuki />
   </div>
   <RouterView />
 </template>
---
+++ src/views/pages/LiaiGroup/LiaiGroupHome.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Hero from '@/views/sections/LiaiGroup/Hero.vue';
+
+</script>
+
+<template>
+  <div>
+    <Hero/>
+    <!-- 狸貓白胖肥龍
+    <RouterLink to="/tanuki" class="">狸貓真相</RouterLink> -->
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
